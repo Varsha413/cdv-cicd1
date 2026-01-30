85e2538af87df322d2d2a3cd5abd9d50f8344639
Prathibhashree R <prathibhashree2004@gmail.com>
2026-01-30 21:06:24 +0530

    Update input prompts for clarity

--- calculator.java
+++ calculator.java
@@ -4,10 +4,10 @@ public class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
+        System.out.print("Enter the first number: ");
         int a = sc.nextInt();
 
-        System.out.print("Enter second number: ");
+        System.out.print("Enter the second number: ");
         int b = sc.nextInt();
 
         int sum = a + b;
