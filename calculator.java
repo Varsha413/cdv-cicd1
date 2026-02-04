20aba20cd33804dd8af8c7b60e7d94ec4e55c0f2
Bindu5112005 <bindukrishnamurthyid@gmail.com>
2026-02-04 11:08:28 +0530

    Update

--- calculator.java
+++ calculator.java
@@ -4,7 +4,7 @@ public class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter  first number: ");
         int a = sc.nextInt();
 
         System.out.print("Enter the second number: ");
